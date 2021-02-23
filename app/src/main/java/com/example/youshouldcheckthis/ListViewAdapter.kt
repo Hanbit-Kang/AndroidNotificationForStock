b4c762ee8009175f5920edacc43a0540912bd1e0
hanbit-kang <fchopinof99@gmail.com>
2021-02-23 21:26:56 +0900

    LongClick - fab icon Animation, Remove 기능 구현

--- app/src/main/java/com/example/youshouldcheckthis/ListViewAdapter.kt
+++ app/src/main/java/com/example/youshouldcheckthis/ListViewAdapter.kt
@@ -1,19 +1,28 @@
 package com.example.youshouldcheckthis
 
+ import android.animation.ValueAnimator
  import android.content.Context
  import android.graphics.Color
+ import android.text.Layout
  import android.util.Log
  import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
+ import android.view.ViewParent
+ import android.view.animation.Animation
+ import android.view.animation.RotateAnimation
+ import android.widget.BaseAdapter
  import android.widget.CheckBox
  import android.widget.LinearLayout
  import android.widget.TextView
  import androidx.core.content.ContextCompat
+ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListViewAdapter : BaseAdapter(){
     private var listViewItemList = ArrayList<ListViewItem>()
+    public var isRemoveMode = false
+    private lateinit var viewGroupParent:ViewGroup
+    public lateinit var rootView:View
 
     override fun getCount(): Int{
         return listViewItemList.size
@@ -22,6 +31,7 @@ class ListViewAdapter : BaseAdapter(){
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var view = convertView
         val context = parent.context
+        this.viewGroupParent = parent
 
         if (view==null){
             val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -48,12 +58,8 @@ class ListViewAdapter : BaseAdapter(){
         //LongClick -> edit mode
         val listviewItem = view.findViewById<LinearLayout>(R.id.listview_item)
         listviewItem.setOnLongClickListener(View.OnLongClickListener{
-            var i:Int= 0
-            for(i in 0 until this.count){
-                val curView:View = parent.getChildAt(i)
-                val checkboxLayout = curView.findViewById<LinearLayout>(R.id.checkbox_layout)
-                checkboxLayout.visibility = View.VISIBLE
-            }
+            this.setCheckBoxVisible()
+            this.isRemoveMode = true
             val checkbox = view.findViewById<CheckBox>(R.id.checkbox)
             checkbox.isChecked = true
             true
@@ -86,5 +92,55 @@ class ListViewAdapter : BaseAdapter(){
         item.stockRateStr = stockRate
 
         listViewItemList.add(item)
+    }
+
+    fun removeItem(index:Int){
+        listViewItemList.removeAt(index)
+    }
+
+    fun setCheckBoxVisible(){
+        var i:Int= 0
+        for(i in 0 until this.count){
+            val curView:View = this.viewGroupParent.getChildAt(i)
+            val checkboxLayout = curView.findViewById<LinearLayout>(R.id.checkbox_layout)
+            checkboxLayout.visibility = View.VISIBLE
+        }
+
+        var fabRemove = this.rootView.findViewById<FloatingActionButton>(R.id.fab_remove)
+        fabRemove.visibility = View.VISIBLE
+
+        var fabAdd = this.rootView.findViewById<FloatingActionButton>(R.id.fab_add)
+        val animationRotate45Degree: RotateAnimation = RotateAnimation(
+                0f,
+                45f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f
+        )
+        animationRotate45Degree.duration = 250
+        animationRotate45Degree.fillAfter = true
+        fabAdd.startAnimation(animationRotate45Degree)
+    }
+
+    fun setCheckBoxInvisible(){
+        var i:Int= 0
+        for(i in 0 until this.count){
+            val curView:View = this.viewGroupParent.getChildAt(i)
+            val checkboxLayout = curView.findViewById<LinearLayout>(R.id.checkbox_layout)
+            val checkbox = curView.findViewById<CheckBox>(R.id.checkbox)
+            checkboxLayout.visibility = View.INVISIBLE
+            checkbox.isChecked = false
+        }
+        var fabRemove = this.rootView.findViewById<FloatingActionButton>(R.id.fab_remove)
+        fabRemove.visibility = View.INVISIBLE
+        var fabAdd = this.rootView.findViewById<FloatingActionButton>(R.id.fab_add)
+        val animationRotate45Degree: RotateAnimation = RotateAnimation(
+                45f,
+                0f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f
+        )
+        animationRotate45Degree.duration = 250
+        animationRotate45Degree.fillAfter = true
+        fabAdd.startAnimation(animationRotate45Degree)
     }
 }
--- app/src/main/java/com/example/youshouldcheckthis/MainActivity.kt
+++ app/src/main/java/com/example/youshouldcheckthis/MainActivity.kt
@@ -9,10 +9,10 @@ import android.util.Log
 import android.view.View
 import android.view.View.OnFocusChangeListener
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
 import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.ListView
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.iterator
@@ -25,14 +25,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // ActionBar Customize
-        supportActionBar?.title = "주식 목록"
+        supportActionBar?.title = "관심 종목"
 
         //ListView
-        val listview: ListView
-        val adapter: ListViewAdapter
-
-        adapter = ListViewAdapter()
-        listview = findViewById<View>(R.id.stock_list_view) as ListView
+        val listview: ListView = findViewById<View>(R.id.stock_list_view) as ListView
+        val adapter: ListViewAdapter = ListViewAdapter()
+        adapter.rootView = findViewById<View>(android.R.id.content).rootView
         listview.adapter = adapter
 
         //Default
@@ -41,25 +39,44 @@ class MainActivity : AppCompatActivity() {
         adapter.addItem("SK하이닉스", "136500", "2.63")
         adapter.addItem("삼성전자", "82200", "-0.48")
 
-        // + Button -> Dialog!
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
-            val builder = AlertDialog.Builder(this)
-            val dialogView = layoutInflater.inflate(R.layout.custom_dialog, null)
-            val dialogText = dialogView.findViewById<EditText>(R.id.dialogEditText)
-            val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            builder.setView(dialogView)
-                    .setNegativeButton("취소") {dialog: DialogInterface?, which: Int ->
-                        imm.hideSoftInputFromWindow(dialogText.windowToken, 0)
-                    }
-                    .setPositiveButton("확인"){ dialog: DialogInterface?, which: Int ->
-                        adapter.addItem(dialogText.text.toString(), "0", "0")
-                        imm.hideSoftInputFromWindow(dialogText.windowToken, 0)
-                    }
-                    .setCancelable(false)
-                    .show()
-            Handler(Looper.getMainLooper()).postDelayed({
-                imm.showSoftInput(dialogText, InputMethodManager.SHOW_FORCED)
-            }, 200L)
+        // + / x Btn
+        findViewById<FloatingActionButton>(R.id.fab_add).setOnClickListener {
+            if(adapter.isRemoveMode){ // x Btn -> Cancel
+                adapter.isRemoveMode= false
+                adapter.setCheckBoxInvisible()
+            }else{ // + Btn -> Add
+                val builder = AlertDialog.Builder(this)
+                val dialogView = layoutInflater.inflate(R.layout.custom_dialog, null)
+                val dialogText = dialogView.findViewById<EditText>(R.id.dialogEditText)
+                val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                builder.setView(dialogView)
+                        .setNegativeButton("취소") {dialog: DialogInterface?, which: Int ->
+                            imm.hideSoftInputFromWindow(dialogText.windowToken, 0)
+                        }
+                        .setPositiveButton("확인"){ dialog: DialogInterface?, which: Int ->
+                            adapter.addItem(dialogText.text.toString(), "0", "0")
+                            imm.hideSoftInputFromWindow(dialogText.windowToken, 0)
+                        }
+                        .setCancelable(false)
+                        .show()
+                Handler(Looper.getMainLooper()).postDelayed({
+                    imm.showSoftInput(dialogText, InputMethodManager.SHOW_FORCED)
+                }, 200L)
+            }
+        }
+
+        //Remove Btn
+        findViewById<FloatingActionButton>(R.id.fab_remove).setOnClickListener {
+            var i:Int= 0
+            for(i in listview.count-1 downTo 0){
+                val curCheckBox = listview.getChildAt(i).findViewById<CheckBox>(R.id.checkbox)
+                if(curCheckBox.isChecked){
+                    adapter.removeItem(i)
+                }
+            }
+            adapter.isRemoveMode = false
+            adapter.setCheckBoxInvisible()
+            adapter.notifyDataSetChanged()
         }
     }
 }
